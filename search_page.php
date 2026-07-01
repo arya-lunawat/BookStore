97421b014efec4945fd998c35c05f53cde6d1672
Arya Lunawat <aryalunawatwork@gmail.com>
2026-07-01 22:32:22 +0530

    Added public domain books

--- search_page.php
+++ search_page.php
@@ -35,8 +35,6 @@ if (isset($_POST['add_to_cart'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Search Page</title>
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
   <link rel="stylesheet" href="style1.css">
   <link rel="stylesheet" href="home1.css">
   <style>
@@ -109,6 +107,9 @@ if (isset($_POST['add_to_cart'])) {
           echo '<p class="empty">No result found!</p>';
         }
       }
+    } else {
+      echo '<p class="empty">Search Something!</p>';
+    }
       ?>
     </div>
   </section>
@@ -116,7 +117,6 @@ if (isset($_POST['add_to_cart'])) {
   <?php
   include 'footer.php';
   ?>
-  <script src="https://kit.fontawesome.com/eedbcd0c96.js" crossorigin="anonymous"></script>
 
   <script src="script.js"></script>
 
